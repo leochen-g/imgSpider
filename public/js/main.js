c651b3c2e57fca459dad87106d5822dd8b8aefde
chengeng <chengeng@bloggeng.com>
2020-03-11 13:55:14 +0800

    添加本地下载支持，不再存放服务器

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,54 @@
+var FunLib = {
+	// 图片打包下载
+	download: function (images) {
+		FunLib.packageImages(images)
+	},
+	// 打包压缩图片
+	packageImages: function (imgs) {
+		var imgBase64 = []
+		var imageSuffix = [] // 图片后缀
+		var zip = new JSZip()
+		var img = zip.folder("images")
+
+		for (var i = 0; i < imgs.length; i++) {
+			var src = imgs[i].url
+			var suffix = src.substring(src.lastIndexOf("."))
+			imageSuffix.push(suffix)
+			FunLib.getBase64(imgs[i].url).then(function (base64) {
+				imgBase64.push(base64.substring(22))
+				if (imgs.length === imgBase64.length) {
+					for (var i = 0; i < imgs.length; i++) {
+						img.file(imgs[i].name + imageSuffix[i], imgBase64[i], {base64: true})
+					}
+					zip.generateAsync({type: "blob"}).then(function (content) {
+						saveAs(content, "images.zip")
+					})
+				}
+			}, function (err) {
+				console.log(err)
+			})
+		}
+	},
+	// 传入图片路径，返回base64
+	getBase64: function (img) {
+		var image = new Image()
+		image.crossOrigin = 'Anonymous'
+		image.src = img
+		var deferred = $.Deferred()
+		if (img) {
+			image.onload = function () {
+				var canvas = document.createElement("canvas")
+				canvas.width = image.width
+				canvas.height = image.height
+				var ctx = canvas.getContext("2d")
+				ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+				var dataURL = canvas.toDataURL()
+				deferred.resolve(dataURL)
+			}
+			return deferred.promise()
+		}
+	}
+}
 $(function () {
   let fileName
   $('.dropdown-toggle').dropdown()
